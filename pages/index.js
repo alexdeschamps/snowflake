a992c66833b7381599a992b3cb2a0f4d67388b70
Cale Shapera <caleshapera@gmail.com>
2019-01-17 11:48:16 -0800

    login

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,42 @@
-import SnowflakeApp from '../components/SnowflakeApp'
+import SnowflakeApp from '../components/SnowflakeApp';
+import React, { Component } from 'react';
 
-export default () => <div>
-  <SnowflakeApp />
-</div>
+class index extends Component {
+	constructor() {
+		super()
+			this.state = {
+				isAuthenticated: false
+			}
+		this.logout = this.logout.bind(this);
+	}
+	logout = () => {
+	    this.setState({isAuthenticated: false});
+	    console.log('logging out');
+	  };
+	login = () => {
+		this.setState({isAuthenticated: true});
+	}
+
+	 render() {
+	 	if (this.state.isAuthenticated == false) {
+	 		return(
+	 			<div>
+	 				<button onClick={this.login}>
+	 					Log in
+	 				</button>
+	 			</div>
+	 		)
+	 	} else {
+	 		return(
+		 		<div>
+		 			<SnowflakeApp logout ={this.logout} />
+		 		</div>
+	 		)
+	 	}
+	 }
+}
+
+
+
+
+export default index;
