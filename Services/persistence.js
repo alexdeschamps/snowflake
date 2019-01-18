a4c1e0794adf81d6e8b5745cd61e062a3c50fc19
Kitty Tang <kitty.tang@clio.com>
2019-01-17 16:54:08 -0800

    Generate mileston map from constant file

--- Services/persistence.js
+++ Services/persistence.js
@@ -1,6 +1,8 @@
+// @flow 
 import { local } from "d3-selection";
+import { trackIds } from '../constants'
 
-export const getUserTrack= (userId: string): any => {
+export const getUserTrack = (userId: string): any => {
     // return dummy data for now
     if (window.localStorage && "milestoneTrack" in window.localStorage) {
         return JSON.parse(window.localStorage.getItem("milestoneTrack"));
@@ -8,28 +10,16 @@ export const getUserTrack= (userId: string): any => {
 
     return {
         title: 'Software Developer',
-        milestoneByTrack: {
-          'DOMAIN_KNOWLEDGE': 2,
-          'CUSTOMER_EMPATHY': 3,
-          'EXECUTION': 1,
-          'CODING': 0,
-          'FOSTERING_TECHNICAL_EXCELLENCE': 1,
-          'QUALITY_CRAFTMANSHIP': 0,
-          'PROBLEM_SOLVING': 2,
-          'DESIGN_ARCHITECTURE': 2,
-          'JUDGEMENT_WISDOM': 0,
-          'INFLUENCE': 2,
-          'STEWARDSHIP': 3,
-          'COLLABORATION': 0,
-          'OUTCOME_DRIVEN': 0,
-          'MENTORSHIP': 5,
-          'ADAPTABILITY': 0,
-          'ENGAGEMENT_WITH_OTHER_TEAMS': 1,
-          'EXPECTATION_MANAGEMENT': 2,
-          'EXECUTIVE_ALIGNMENT':  0,
-          'PROCESS_AGILITY':  0,
-        }
+        milestoneByTrack: sampleMilestonMapGenerator(),
     }
+}
+
+const sampleMilestonMapGenerator = () => {
+    const sampleMilestone = {}
+    trackIds.map((track) => {
+        sampleMilestone[track] = Math.floor(Math.random() * 5);
+    });
+    return sampleMilestone;
 }
 
 export const saveUserTrack= ( userId: string, userTrack: object): any => {
